57ecd9f0692b59fbd4e40b3c3ce62abad164cd9e
Metin Keleş <metinkeles677@gmail.com>
2023-05-22 15:16:51 +0300

    ngForDirective

--- Directives/src/app/app.component.ts
+++ Directives/src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+
+    <!-- ngFor directives -->
+    <ul>
+      <li *ngFor="let item of names;
+                                    let i = index;
+                                    let isFirst = first;
+                                    let isLast = last;
+                                    let isEven = even;
+                                    let isOdd = odd;">{{i}}, {{item}}, {{isFirst}}, {{isLast}},{{isEven}},{{isOdd}}</li>
+    </ul>
+
+
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Directives';
+  names: string[] = ["name1", "name2", "name3", "name4"]
+  visible: boolean = false;
+  sayi: number = 1;
 }
