5436368abc16f1e0c17401a53f24adb800bc4f58
Metin Keleş <metinkeles677@gmail.com>
2023-05-22 12:05:21 +0300

    Two-wayDataBinding

--- DataBinding/src/app/app.component.ts
+++ DataBinding/src/app/app.component.ts
@@ -4,10 +4,25 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
-    <app-home [pageName]="title"></app-home>
+    <!-- Property binding -->
+    <!-- <app-home [pageName]="title"></app-home> -->
+
+    <!-- Event Binding -->
+    <!-- <button (click)="btnClick()">Click</button> -->
+
+    <!-- Two-Way Data Binding -->
+    <input type="text" [(ngModel)] = "name"><br><br>
+    <input type="text" [(ngModel)] = "name"><br><br>
+    {{name}}
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'DataBinding';
+
+  name: string;
+
+  btnClick() {
+    alert("click");
+  }
 }
--- DataBinding/src/app/home/home.component.ts
+++ DataBinding/src/app/home/home.component.ts
@@ -5,14 +5,13 @@ import { Component, Input } from '@angular/core';
   // templateUrl: './home.component.html',
   template: `
     <h1>{{pageName}}</h1>
-    <button (click)="btnClick()">Click</button>
+
+
   `,
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   @Input() pageName: string;
 
-  btnClick() {
-    alert("click");
-  }
+
 }
