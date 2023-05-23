35b996075d897e5ebeb6011dd6ea2ca5928994bf
Metin Keleş <metinkeles677@gmail.com>
2023-05-23 20:03:49 +0300

    LifeCycleHook

--- ComponentLifeCycleHook/src/app/app.component.ts
+++ ComponentLifeCycleHook/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-example data="merhaba"></app-example>
+    <app-example data="merhaba">ngcontent</app-example>
   `,
 })
 export class AppComponent {
