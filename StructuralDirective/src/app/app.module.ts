ef460c5a567bc5b26b7ea9af3a80aed2881faa41
Metin Keleş <metinkeles677@gmail.com>
2023-05-23 09:24:51 +0300

    CustomIfDirective

--- StructuralDirective/src/app/app.module.ts
+++ StructuralDirective/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomifDirective } from './directives/customif.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomifDirective
   ],
   imports: [
     BrowserModule,
