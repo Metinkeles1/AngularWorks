8d3f5e45129c35ba66b238666c17a78f51053c12
Metin Keleş <metinkeles677@gmail.com>
2023-05-23 13:36:02 +0300

    ParentToChildCommunication

--- ComponentCommunication/src/app/components/parent_to_child_communication/child/child.component.ts
+++ ComponentCommunication/src/app/components/parent_to_child_communication/child/child.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss']
+  template: `
+  <p>child works!</p>
+  {{childData}}
+  `,
 })
 export class ChildComponent {
-
+  @Input() childData: any;
 }
