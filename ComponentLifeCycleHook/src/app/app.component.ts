56dc72f3d9178f81aa5cad6466778fcaa5bf640b
Metin Keleş <metinkeles677@gmail.com>
2023-05-23 19:19:33 +0300

    CreateProject

---
+++ ComponentLifeCycleHook/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-example></app-example>
+  `,
+})
+export class AppComponent {
+  title = 'ComponentLifeCycleHook';
+}
